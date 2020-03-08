c7fd6cab5048eea8c5a58fa2c36d89e6888bdbe1
dasosjt <diegososajt@gmail.com>
2020-03-08 00:29:31 -0600

    update mongo database credentials

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 //LINK de la base de datos
 module.exports = {
   //mongodb://<dbuser>:<dbpassword>@ds139430.mlab.com:39430/heroku_sn8mvdwb formto de mongodb 
-  url : 'mongodb://diego:hci@ds139791.mlab.com:39791/heroku_hq6dcx7s'``
+  url : 'mongodb://diego:hci@ds139791.mlab.com:39791/heroku_hq6dcx7s'
 }
